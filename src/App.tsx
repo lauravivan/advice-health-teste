3add9d5b1e61f32c9dfb70f75ed883fd13f33495
lauravivan <lauvivang@gmail.com>
2026-08-17 15:31:07 -0300

    chore: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1>
+      Example heading <span className="badge bg-secondary">New</span>
+    </h1>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import "./assets/scss/styles.scss";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
