c3aa44ab3971d3ba2710ca280773a64973f6f4d2
lauravivan <lauvivang@gmail.com>
2026-08-18 11:33:37 -0300

    style(AddEditProfessional.tsx): update layout & add image

--- src/pages/professional/AddEditProfessional.tsx
+++ src/pages/professional/AddEditProfessional.tsx
@@ -1,6 +1,7 @@
 import navigation from "@/navigation";
 import useProfessionalStore from "@/store/professionalStore";
 import { useNavigate } from "react-router";
+import DoctorsImg from "@/assets/undraw_doctors_djoj.svg";
 
 const AddEditProfessional = () => {
   const { createProfessional } = useProfessionalStore();
@@ -30,32 +31,43 @@ const AddEditProfessional = () => {
 
   return (
     <div className="d-flex flex-column w-100 h-100">
-      <h2>Cadastrar profissional</h2>
-      <form onSubmit={handleSubmit} className="mt-3 d-flex flex-column gap-3">
-        <input
-          name="full-name"
-          id="full-name"
-          className="form-control"
-          placeholder="Nome completo"
-        />
-        <div className="d-flex gap-3">
-          <input
-            name="crm"
-            id="crm"
-            className="form-control"
-            placeholder="CRM"
-          />
-          <input
-            name="specialty"
-            id="specialty"
-            className="form-control"
-            placeholder="Especialidade"
-          />
+      <h2 className="text-center mt-3">Cadastrar profissional</h2>
+      <div className="d-flex w-100 h-100">
+        <div className="flex-fill w-75 m-auto bg-light rounded p-3">
+          <p className="fst-italic">Preencha as informações da pessoa profissional</p>
+          <form
+            onSubmit={handleSubmit}
+            className="mt-3 d-flex flex-column gap-3 flex-fill"
+          >
+            <input
+              name="full-name"
+              id="full-name"
+              className="form-control"
+              placeholder="Nome completo"
+            />
+            <div className="d-flex gap-3">
+              <input
+                name="crm"
+                id="crm"
+                className="form-control"
+                placeholder="CRM"
+              />
+              <input
+                name="specialty"
+                id="specialty"
+                className="form-control"
+                placeholder="Especialidade"
+              />
+            </div>
+            <button type="submit" className="me-auto btn btn-primary">
+              Cadastrar
+            </button>
+          </form>
         </div>
-        <button type="submit" className="me-auto btn btn-light">
-          Cadastrar
-        </button>
-      </form>
+        <div className="h-auto flex-fill ms-auto d-flex align-items-end justify-content-end">
+          <img className="img-fluid object-fit-contain" src={DoctorsImg} />
+        </div>
+      </div>
     </div>
   );
 };
